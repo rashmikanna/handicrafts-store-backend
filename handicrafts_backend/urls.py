1a0be4b51237e38ddf315b3bd1978fde2dfc37d8
Arya Malladi <malladiarya379@gmail.com>
2025-05-17 17:40:01 +0530

    Update urls.py

--- handicrafts_backend/urls.py
+++ handicrafts_backend/urls.py
@@ -1,32 +1,31 @@
-"""
-URL configuration for handicrafts project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# JWT views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# Your custom signup view
+from accounts.views import signup
+
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
-    path('api/', include('nosql_products.urls')),
-    path('products/', include('nosql_products.urls')),
+
+    # Auth / Accounts
+    path('api/signup/', signup, name='signup'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # App APIs
+    path('api/', include('producer.urls')),
     path('api/users/', include('nosql_users.urls')),
     path('api/notifications/', include('nosql_notifications.urls')),
+    path('api/products/', include('nosql_products.urls')),  # changed from just 'products/' to 'api/products/'
 ]
 
+# Serve static and media files in development
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
